2710f05fb5385cc77b8a9c7361d6271c02418bea
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-03-27 09:59:03 +0100

    clean up main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-var map = L.map('map').setView([-39.2, 175.583333], 11);
+let lat = -39.2;
+let lng = 175.583333;
+let zoom = 11;
+
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-39.2, 175.583333]).addTo(map);
-marker.bindPopup("<h2>Hello world!</h2><br>I am a popup.").openPopup();
+var marker = L.marker([lat, lng]).addTo(map);
+marker.bindPopup(`
+    <h2>Tongariro Nationalpark</h2>
+    <ul>
+        <li>Breite: ${lat.toFixed(5)}°</li>
+        <li>Länge: ${lng.toFixed(5)}°</li>
+    </ul>
+`).openPopup();
